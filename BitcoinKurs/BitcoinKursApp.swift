2186049ab40abc13072d81d7bfe56a5eacc51b04
Biomass2857 <wasserfrosch1234@gmail.com>
2023-04-14 16:32:51 +0200

    Initial Commit

---
+++ BitcoinKurs/BitcoinKursApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinKursApp.swift
+//  BitcoinKurs
+//
+//  Created by Tom Reinhardt on 14.04.23.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinKursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
